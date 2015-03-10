1c611a35d30c4824bc076c486b2c6f280ad8faf0
gerpad <german.paduan@rosariosoftware.com>
2015-03-10 18:55:10 -0300

    #4 limpiando codigo en Leer_Peso.cs

--- Utilidades/Leer_Peso.cs
+++ Utilidades/Leer_Peso.cs
@@ -12,47 +12,42 @@ namespace Utilidades
 {
     public class Leer_Peso
     {
+        //BasculaCom.PortName = "COM1";
+        //BasculaCom.BaudRate = 9600;
+        //BasculaCom.Parity = System.IO.Ports.Parity.None;
+        //BasculaCom.StopBits = StopBits.One;
+        SerialPort BasculaCom = new SerialPort("COM1", 9600, System.IO.Ports.Parity.None, 8, StopBits.One);
         Label Recibidos = new Label();
         string pesoBalanza = "";
-        SerialPort BasculaCom = new SerialPort("COM1", 9600, System.IO.Ports.Parity.None, 8, StopBits.One);
-
-
-
         internal delegate void MostrarRecepcion(string Texto);
-
-
+        
         public void AbrirPuerto()
         {
             try
             {
-                //BasculaCom.PortName = "COM1";//+ txtNroPuerto.Text.Trim();
-
-                //BasculaCom.BaudRate = 9600;
-
-                //BasculaCom.Parity = System.IO.Ports.Parity.None;
-
-                //BasculaCom.StopBits = StopBits.One;
-
                 if (!BasculaCom.IsOpen)
                 {
-                    //ComunicacionBalanza();
                     BasculaCom.Open();
-                    MessageBox.Show("abre puerto en AbrirPuerto()   ");
                 }
-                MessageBox.Show("ultima linea try en AbrirPuerto()   ");
             }
             catch (Exception ex)
             {
-
-                MessageBox.Show("Exception en AbrirPuerto()   " + ex.Message);
+                MessageBox.Show("Exception en AbrirPuerto() \n" + ex.Message);
             }
         }
 
         public void CerrarPuerto()
-        {
-            if (BasculaCom.IsOpen)
+        {            
+            try
             {
-                BasculaCom.Close();
+                if (BasculaCom.IsOpen)
+                {
+                    BasculaCom.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception en CerrarPuerto() \n" + ex.Message);
             }
         }
 
@@ -61,71 +56,47 @@ namespace Utilidades
             try
             {
                 AbrirPuerto();
-                MessageBox.Show("Ingreso al Try");
-                //enviar una p
+
                 byte[] miBuffer = new byte[1];
                 miBuffer[0] = 5;
                 BasculaCom.Write(miBuffer, 0, miBuffer.Length);
-                MessageBox.Show("ReadExisting()  " + BasculaCom.ReadExisting());
                 MostrarRecibidos(BasculaCom.ReadExisting());
             }
             catch (Exception ex)
             {
-
-                MessageBox.Show("ObtenerPeso  EXception  ->  " + ex.Message);
-
+                MessageBox.Show("Exception en ObtenerPeso \n" + ex.Message);
             }
             return pesoBalanza;
         }
 
         internal void Recibir()
         {
-
             //al recibir de la bascula los bytesToRead indicara
-
             //un valor superior a 0
-
-
             MessageBox.Show("  Recibir()");
             try
             {
-                //byte[] byteRecibidos= StrToByteArray(BasculaCom.ReadExisting());
-                //lblCantBytes.Text = byteRecibidos.Length.ToString();
                 MostrarRecibidos(BasculaCom.ReadExisting());
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Recibir  " + ex.Message);
+                MessageBox.Show("Recibir  \n" + ex.Message);
             }
         }
 
-
-
         private void MostrarRecibidos(string texto)
         {
-            MessageBox.Show("  MostrarRecibidos - Try - Con parametro string = " + texto + "  <- param");
             try
             {
-
-
                 //Mostrar los bytes recibidos en el Label recibidos
                 //BasculaCom.InvokeRequired
-
                 if (Recibidos.InvokeRequired)
                 {
-
                     MostrarRecepcion delegado = new MostrarRecepcion(MostrarRecibidos);
-
                     this.Invoke(delegado, new object[] { texto });
-
                 }
                 else
                 {
-                    //byte[] p = new byte[] { 49, 50, 51, 52, 53, 54 };  // Simulamos un byte[] con la codificacion ASCII de la cadena 123456
-                    //System.Text.Encoding encoding = System.Text.Encoding.ASCII;
-                    //this.Recibidos.Text = texto;
-                    //string texto = encoding.GetString(p); // Tenemos la cade 123456
-
                     string peso = "";
                     char[] nuevoPeso;
 
@@ -133,7 +104,6 @@ namespace Utilidades
                     {
                         peso = "Error - " + texto;
                     }
-
                     else
                     {
                         int contar = 0; //cuenta los dígitos usables
@@ -141,7 +111,6 @@ namespace Utilidades
                         bool esNegativo = false; //si es negativo el peso se estable true
                         foreach (char letra in texto)
                         {
-
                             if (letra == '-')
                             {
                                 indices[contar] = letra;
@@ -174,14 +143,11 @@ namespace Utilidades
                     pesoBalanza = peso;
                     Recibidos.Text = peso;
                 }
-
             }
             catch (Exception ex)
             {
-                MessageBox.Show("MostrarRecibidos  " + ex.Message);
+                MessageBox.Show("Exception en MostrarRecibidos() \n" + ex.Message);
             }
-
-
         }
 
         private void Invoke(MostrarRecepcion delegado, object[] p)
@@ -191,10 +157,7 @@ namespace Utilidades
 
         private void BasculaCom_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-            MessageBox.Show("BasculaCom_DataReceived  ");
             Recibir();
         }
-
-
     }
 }
