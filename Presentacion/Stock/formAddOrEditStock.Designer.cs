a05eca7523685be748d198129222aeffd10126ed
gerpad <german.paduan@rosariosoftware.com>
2015-05-18 20:27:37 -0300

    readonly al campo kg en FormaddOrEditStock

--- Presentacion/Stock/formAddOrEditStock.Designer.cs
+++ Presentacion/Stock/formAddOrEditStock.Designer.cs
@@ -139,6 +139,7 @@
             this.txtCantKgs.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtCantKgs.Location = new System.Drawing.Point(416, 18);
             this.txtCantKgs.Name = "txtCantKgs";
+            this.txtCantKgs.ReadOnly = true;
             this.txtCantKgs.Size = new System.Drawing.Size(89, 22);
             this.txtCantKgs.TabIndex = 1;
             this.txtCantKgs.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
