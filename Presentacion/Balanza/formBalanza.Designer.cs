5ac60ba597613d69394460ef210915ca70939c33
gerpad <german.paduan@rosariosoftware.com>
2015-09-03 19:00:38 -0300

    Agregando menu Balanza para pruebas de lectura

---
+++ Presentacion/Balanza/formBalanza.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Presentacion.Balanza
+{
+    partial class formBalanza
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtVelocidadTimer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPesoBalanza = new System.Windows.Forms.TextBox();
+            this.btnTimer = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnVerBalanza = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Velocidad Timer";
+            // 
+            // txtVelocidadTimer
+            // 
+            this.txtVelocidadTimer.Location = new System.Drawing.Point(124, 25);
+            this.txtVelocidadTimer.Name = "txtVelocidadTimer";
+            this.txtVelocidadTimer.Size = new System.Drawing.Size(75, 20);
+            this.txtVelocidadTimer.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Peso balanza";
+            // 
+            // txtPesoBalanza
+            // 
+            this.txtPesoBalanza.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPesoBalanza.Location = new System.Drawing.Point(124, 51);
+            this.txtPesoBalanza.Name = "txtPesoBalanza";
+            this.txtPesoBalanza.Size = new System.Drawing.Size(156, 29);
+            this.txtPesoBalanza.TabIndex = 7;
+            // 
+            // btnTimer
+            // 
+            this.btnTimer.Location = new System.Drawing.Point(205, 22);
+            this.btnTimer.Name = "btnTimer";
+            this.btnTimer.Size = new System.Drawing.Size(75, 24);
+            this.btnTimer.TabIndex = 6;
+            this.btnTimer.Text = "Timer";
+            this.btnTimer.UseVisualStyleBackColor = true;
+            this.btnTimer.Click += new System.EventHandler(this.btnTimer_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnVerBalanza
+            // 
+            this.btnVerBalanza.Location = new System.Drawing.Point(285, 106);
+            this.btnVerBalanza.Name = "btnVerBalanza";
+            this.btnVerBalanza.Size = new System.Drawing.Size(75, 24);
+            this.btnVerBalanza.TabIndex = 11;
+            this.btnVerBalanza.Text = "Ver balanza";
+            this.btnVerBalanza.UseVisualStyleBackColor = true;
+            this.btnVerBalanza.Click += new System.EventHandler(this.btnVerBalanza_Click);
+            // 
+            // formBalanza
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 142);
+            this.Controls.Add(this.btnVerBalanza);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtVelocidadTimer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPesoBalanza);
+            this.Controls.Add(this.btnTimer);
+            this.Name = "formBalanza";
+            this.Text = "Balanza";
+            this.Load += new System.EventHandler(this.formBalanza_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtVelocidadTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPesoBalanza;
+        private System.Windows.Forms.Button btnTimer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnVerBalanza;
+    }
+}
---
+++ Presentacion/Balanza/formBalanza.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Presentacion.Balanza
+{
+    public partial class formBalanza : Form
+    {
+
+        Utilidades.FormLeer_Peso Leer_Peso = Utilidades.FormLeer_Peso.CrearLeerPeso();
+
+        public formBalanza()
+        {
+            InitializeComponent();
+        }
+
+        private void formBalanza_Load(object sender, EventArgs e)
+        {
+            txtVelocidadTimer.Text = timer1.Interval.ToString();
+        }
+
+        private void btnTimer_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                txtVelocidadTimer.ReadOnly = false;
+                btnTimer.Text = "Start";
+            }
+            else
+            {
+                timer1.Interval = Convert.ToInt32(txtVelocidadTimer.Text);
+                timer1.Start();
+                txtVelocidadTimer.ReadOnly = true;
+                btnTimer.Text = "Stop";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            txtPesoBalanza.Text = Leer_Peso.ObtenerPeso();
+        }
+
+        private void btnVerBalanza_Click(object sender, EventArgs e)
+        {
+
+            
+            //Leer_Peso.Show();
+            try
+            {
+                Leer_Peso.Show();
+            }
+            catch (Exception)
+            {
+                //Leer_Peso.Dispose();
+                Leer_Peso = Utilidades.FormLeer_Peso.CrearLeerPeso();
+                Leer_Peso.Show();
+            }
+        }
+    }
+}
