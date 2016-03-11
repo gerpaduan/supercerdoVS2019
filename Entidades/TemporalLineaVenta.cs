ae52df5a629767d42836edc4bbce567e80042ddb
gerpad <german.paduan@rosariosoftware.com>
2016-03-11 20:45:59 -0300

    Agregando Temporal de linea Venta

---
+++ Entidades/TemporalLineaVenta.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entidades
+{
+    public class TemporalLineaVenta
+    {
+
+        DateTime fechaInicioPesada;
+
+        public DateTime FechaInicioPesada
+        {
+            get { return fechaInicioPesada; }
+            set { fechaInicioPesada = value; }
+        }
+
+        Usuario vendedor;
+
+        public Usuario Vendedor
+        {
+            get { return vendedor; }
+            set { vendedor = value; }
+        }
+
+        bool ventaEnCurso;
+
+        public bool VentaEnCurso
+        {
+            get { return ventaEnCurso; }
+            set { ventaEnCurso = value; }
+        }
+
+        Entidades.Sucursal sucursal;
+
+        public Entidades.Sucursal Sucursal
+        {
+            get { return sucursal; }
+            set { sucursal = value; }
+        }
+
+         float cantKg;
+         float totalCorte;
+
+         public float TotalCorte
+         {
+             get { return totalCorte; }
+             set { totalCorte = value; }
+         }
+         Corte corte;
+
+        public float CantKg
+        {
+            get
+            {
+                return cantKg;
+            }
+            set
+            {
+                cantKg = value;
+            }
+        }
+
+        public Corte Corte
+        {
+            get
+            {
+                return corte;
+            }
+            set
+            {
+                corte = value;
+            }
+        }
+    }
+}
