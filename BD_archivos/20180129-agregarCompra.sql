052aca138146224d3eb239f5917cd4f08cf8bcac
gerpad <german.paduan@rosariosoftware.com>
2018-01-30 12:59:38 -0300

    Store procedure para AjusteStock

---
+++ BD_archivos/20180129-agregarCompra.sql
@@ -0,0 +1,4 @@
+use SuperCerdo
+SELECT idCompra FROM dbo.Compras WHERE tipoCompra like 'Ajuste Stock' --AND nroRemito like '2694'
+--nroRemito = '2694' and tipoCompra = 'Ajuste Stock'
+--tipoCompra like 'Ajuste Stock' --AND nroRemito like '2694'
