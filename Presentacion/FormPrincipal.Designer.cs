13a3e534636cb90ff36e05e8cceaf40596f1fecc
German <germanpaduan@gmail.com>
2024-08-02 17:45:54 -0300

    imagen carnisys en menu

--- Presentacion/FormPrincipal.Designer.cs
+++ Presentacion/FormPrincipal.Designer.cs
@@ -640,9 +640,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-7, 1);
+            this.pictureBox1.Location = new System.Drawing.Point(-16, 1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(284, 52);
+            this.pictureBox1.Size = new System.Drawing.Size(287, 52);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 34;
             this.pictureBox1.TabStop = false;
