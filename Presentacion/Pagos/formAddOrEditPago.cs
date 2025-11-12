c30283bcae9209737146ad21cc1188445a989119
gerpaduan <germanpaduan@gmail.com>
2025-11-12 12:00:08 -0300

    limpiando txt implementacion

--- Presentacion/Pagos/formAddOrEditPago.cs
+++ Presentacion/Pagos/formAddOrEditPago.cs
@@ -730,10 +730,10 @@ namespace Presentacion.Pagos
                 }
 
                 //se verifica que el cheque a entregar haya sido recibido o sea Propio
-                if (checkAProveedor.Checked && (oCheque.Propio || (oCheque.PagoDe != null && oCheque.PagoDe.Id > 0)))
+                if (checkAProveedor.Checked && (!oCheque.Propio || (oCheque.PagoDe != null && oCheque.PagoDe.Id > 0)))
                 {
-                    MessageBox.Show("El Cheque a entregar debe de Propio o haber sido recibido en un cobro"+
-                        "\nUd. está tratando de asignar un cheque que no tiene un origen.");
+                    MessageBox.Show("El Cheque a entregar debe de Propio o haber sido recibido de un cobro."+
+                        "\n\nUd. está tratando de asignar un cheque que no tiene un origen.", "", MessageBoxButtons.OK, MessageBoxIcon.Stop);
                     return;
                 }
 
