c88a946c49106435161f72970eff5eb195869343
gerpaduan <germanpaduan@gmail.com>
2026-05-15 12:58:55 -0300

    Muestra detalle tecnico en errores web

--- Web/Controllers/ErrorController.cs
+++ Web/Controllers/ErrorController.cs
@@ -19,6 +19,15 @@ namespace Web.Controllers
             Response.StatusCode = 200;
             Response.TrySkipIisCustomErrors = true;
             ViewBag.RequestedUrl = Request != null ? Request.RawUrl : "";
+            var ex = Server != null ? Server.GetLastError() : null;
+            if (ex != null)
+            {
+                ViewBag.ErrorMessage = ex.Message;
+                ViewBag.ErrorType = ex.GetType().FullName;
+                ViewBag.ErrorStack = ex.StackTrace;
+                ViewBag.InnerErrorMessage = ex.InnerException != null ? ex.InnerException.Message : "";
+                ViewBag.InnerErrorType = ex.InnerException != null ? ex.InnerException.GetType().FullName : "";
+            }
             return View();
         }
     }
